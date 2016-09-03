0387349a7f5fee35652439944708d7a9c0621e5e
ixt <orange@ff4500.red>
2016-09-03 13:44:44 +0100

    fixed some issues with short articles and removed some redundant parts

--- Process.sh
+++ Process.sh
@@ -5,14 +5,14 @@
 sed 1d list.txt | while read entry; do
     lynx --dump -width 1024 http://simple.wikipedia.org/wiki/$entry | sed 's/\[[^[]*\]//g' > .tempsite
     cat .tempsite > .tempwhile
-    NAVIGATION=$((( 1 + $(grep -n "Navigation menu" .tempsite | cut -d ':' -f1 | tail -1))))
     TITLE=$(grep -n "$(echo $entry | sed 's/_/ /g')" .tempsite | cut -d ':' -f1 | head -n2 | tail -1)
-    echo $NAVIGATION $entry
-    cat .tempsite | tail -n+$TITLE | head --lines=$NAVIGATION > .tempwhile
-    
+    if [[ $TITLE ]]; then
+        cat .tempsite | tail -n+$TITLE > .tempwhile
+    fi
     cat .tempwhile | sed -e 's/([^()]*)//g' | sed 's/\[change |//g' | sed 's/\.[a-z]*//g' | sed -e 's/[[:punct:]]//g' > .tempin
 
     ./Haiku.py -i .tempin -o .tempout > outputs/$entry.txt
+    echo $NAVIGATION $TITLE $entry
     cat outputs/$entry.txt
 done
 #rm .tempin .tempout 
